2478947d552024960bbc2ac4e2e2207c5fdc8d30
matt foster <mattfoster01@gmail.com>
2012-10-14 22:57:08 -0400

    Added security stub
    Broadcast operation needs to be restricted, put in a boolean method
    that will make subclassing easier.

--- SocketBundle/Listener/WebsocketBroadcastListener.php
+++ SocketBundle/Listener/WebsocketBroadcastListener.php
@@ -19,12 +19,16 @@ class WebSocketBroadcastListener
     
     public function onMessage(SocketEvent $evt)
     {
-        
+    
         $message = $evt->getMessage();
         
+        if(!$this->hasAuthorization($evt)){
+            $this->logger->warn(sprintf('Received unauthorized attempt to broadcast with message of %s', $message));
+            return false;
+        }
+        
         $this->logger->debug(sprintf('received message of some sort in listener %s', $message));
-        
-        
+                
         $result = json_decode("$message");
         
         $this->logger->debug(sprintf('result of json_decode on message %s', print_r($result, true)));
@@ -36,6 +40,20 @@ class WebSocketBroadcastListener
         
         
         
+    }
+    
+    /**
+     * This method is meant to be subclassed.  Broadcasting is a powerful operation, you should
+     * limit who can perform this action, override the parameter for this service with a subclass
+     * that will handle authorization.
+     * 
+     * @access public
+     * @param SocketEvent $evt
+     * @return void
+     */
+    public function hasAuthorization($evt)
+    {
+        return true;
     }
 
 
